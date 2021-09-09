d40960f1490d067ffa0401f119994c30fddd171a
yuto-kawahara <mashroom1384@gmail.com>
2021-09-09 01:12:06 +0000

    [Finish]Bookers修正完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_014227) do
+ActiveRecord::Schema.define(version: 2021_09_09_003105) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
